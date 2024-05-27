e843e623f68a4f0213b9682ee326d8f67a87848a
Matouš Borák <matous.borak@platanus.cz>
2024-05-27 16:35:29 +0200

    Support hash groups in some steps

--- lib/dump_cleaner/cleanup_data/source_steps/group_by_byte_length.rb
+++ lib/dump_cleaner/cleanup_data/source_steps/group_by_byte_length.rb
@@ -4,8 +4,16 @@ module DumpCleaner
   module CleanupData
     module SourceSteps
       class GroupByByteLength
-        def run(data, type:)
-          data.group_by { "#{_1.length}-#{_1.bytes.length}" }
+        def run(data, type:, hash: false)
+          group_by_lambda = -> { "#{_1.length}-#{_1.bytes.length}" }
+
+          if hash
+            new_data = {}
+            data.each_key { |key| new_data[key] = data[key].group_by(&group_by_lambda) }
+            new_data
+          else
+            data.group_by(&group_by_lambda)
+          end
         end
       end
     end
--- lib/dump_cleaner/cleanup_data/source_steps/load_yaml_file.rb
+++ lib/dump_cleaner/cleanup_data/source_steps/load_yaml_file.rb
@@ -4,8 +4,17 @@ module DumpCleaner
   module CleanupData
     module SourceSteps
       class LoadYamlFile
-        def run(data, type:, file:, key: nil)
-          YAML.load_file(file)
+        def run(data, type:, file:, under_key: nil)
+          loaded_data = YAML.load_file(file)
+
+          new_data = data.dup
+          if under_key
+            new_data ||= {}
+            new_data[under_key] = loaded_data
+            new_data
+          else
+            loaded_data
+          end
         end
       end
     end
