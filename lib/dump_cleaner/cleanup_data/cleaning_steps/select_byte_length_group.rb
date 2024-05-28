e7a34474d5dad2ff0cabf68fd39f796a65b592b9
Matouš Borák <matous.borak@platanus.cz>
2024-05-28 21:08:05 +0200

    Fix selecting from group when mixing accented chars

--- lib/dump_cleaner/cleanup_data/cleaning_steps/select_byte_length_group.rb
+++ lib/dump_cleaner/cleanup_data/cleaning_steps/select_byte_length_group.rb
@@ -5,7 +5,8 @@ module DumpCleaner
     module CleaningSteps
       class SelectByteLengthGroup < Base
         def run(orig_value:, record: {})
-          data["#{orig_value.length}-#{orig_value.bytes.length}"]
+          data["#{orig_value.length}-#{orig_value.bytes.length}"] ||
+            data["#{orig_value.bytes.length}-#{orig_value.bytes.length}"] # used if orig_value is accented but data isn't
         end
       end
     end
