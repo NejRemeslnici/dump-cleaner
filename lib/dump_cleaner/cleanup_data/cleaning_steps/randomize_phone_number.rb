51b6945d1ea131c79ec0de703aebc14291ec0c8d
Matouš Borák <matous.borak@platanus.cz>
2024-05-29 21:11:12 +0200

    Support more phone number formats when randomizing

--- lib/dump_cleaner/cleanup_data/cleaning_steps/randomize_phone_number.rb
+++ lib/dump_cleaner/cleanup_data/cleaning_steps/randomize_phone_number.rb
@@ -4,15 +4,36 @@ module DumpCleaner
   module CleanupData
     module CleaningSteps
       class RandomizePhoneNumber < Base
-        def run(orig_value:, record: {})
-          unless orig_value.length >= 9
+        def run(orig_value:, record: {}, format: '(?<front>\+(?:\d{6}))(?<x>\d{6})')
+          regex = Regexp.new("\\A#{format}\\z")
+
+          unless orig_value.match?(regex)
             warn "ID: #{record['id']} invalid phone number: #{orig_value}"
             return
           end
 
-          prefix = orig_value.slice(0...orig_value.length - 6)
           random = Random.new(Zlib.crc32(orig_value) + repetition)
-          "#{prefix}#{format('%06d', random.rand(999_999))}"
+
+          new_value = String.new
+          orig_value.match(regex).named_captures.each do |name, capture|
+            if name.start_with?("x")
+              unless capture.match?(/^\d+$/)
+                raise "Invalid regexp for capture #{name} which matched to #{capture}: must match numbers only."
+              end
+
+              new_value << random_number(capture.length, random:)
+            else
+              new_value << capture
+            end
+          end
+
+          new_value
+        end
+
+        private
+
+        def random_number(digits, random:)
+          random.rand(10**digits - 1).to_s.rjust(digits, "0")
         end
       end
     end
