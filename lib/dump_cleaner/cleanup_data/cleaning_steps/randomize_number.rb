3efb02cd2cb25be48ab0cc12b401e3a3437b8690
Matouš Borák <matous.borak@platanus.cz>
2024-05-29 16:36:47 +0200

    Align number sign when randomizing

--- lib/dump_cleaner/cleanup_data/cleaning_steps/randomize_number.rb
+++ lib/dump_cleaner/cleanup_data/cleaning_steps/randomize_number.rb
@@ -7,7 +7,9 @@ module DumpCleaner
         def run(orig_value:, record: {}, max_difference: 1.0)
           random = Random.new(Zlib.crc32(orig_value) + repetition)
           spec = "%0.#{orig_value.scan(/\.(.*)$/).first&.first.to_s.length}f"
-          format(spec, orig_value.to_f + random.rand(max_difference * 2 * 1_000_000) / 1_000_000 - max_difference)
+          new_value = orig_value.to_f + random.rand(max_difference * 2 * 1_000_000) / 1_000_000 - max_difference
+          new_value *= -1 if (orig_value.to_f <=> 0) != (new_value <=> 0) # align sign
+          format(spec, new_value)
         end
       end
     end
