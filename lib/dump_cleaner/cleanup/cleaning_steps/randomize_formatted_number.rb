cfa8a771f80529ce220432accb0834de7a5c5ea5
Matouš Borák <matous.borak@platanus.cz>
2024-06-02 18:16:10 +0200

    Add RandomizeFormattedNumber spec

--- lib/dump_cleaner/cleanup/cleaning_steps/randomize_formatted_number.rb
+++ lib/dump_cleaner/cleanup/cleaning_steps/randomize_formatted_number.rb
@@ -7,6 +7,10 @@ module DumpCleaner
         def run(format:)
           regex = Regexp.new("\\A#{format}\\z")
 
+          unless regex.names.any? { _1.start_with?("x") }
+            raise ArgumentError, 'The format has no named group starting with \'x\', e.g. \'(?<x>\d)\')'
+          end
+
           unless current_value.match?(regex)
             Log.warn { "Invalid formatted number: id: #{record['id']}, value: #{current_value}" } if repetition.zero?
             step_context.current_value = nil
@@ -14,12 +18,27 @@ module DumpCleaner
           end
 
           random = Random.new(crc32(current_value:, record:))
+          new_value = randomize_named_captures(regex:, random:)
 
+          if new_value.length != current_value.length
+            raise ArgumentError, "The new value length does not match the original value length.
+                                  Do the named groups in the format regexp match the whole value?".gsub(/\s+/, " ")
+          end
+
+          step_context.current_value = new_value
+          step_context
+        end
+
+        private
+
+        def randomize_named_captures(regex:, random:)
           new_value = String.new
+
           current_value.match(regex).named_captures.each do |name, capture|
             if name.start_with?("x")
               unless capture.match?(/^\d+$/)
-                raise "Invalid regexp for capture #{name} which matched to #{capture}: must match numbers only."
+                raise ArgumentError,
+                      "Invalid regexp for capture '#{name}' which matched to '#{capture}': it must match numbers only."
               end
 
               new_value << random_number(capture.length, random:)
@@ -28,11 +47,8 @@ module DumpCleaner
             end
           end
 
-          step_context.current_value = new_value
-          step_context
+          new_value
         end
-
-        private
 
         def random_number(digits, random:)
           random.rand(10**digits - 1).to_s.rjust(digits, "0")
---
+++ spec/lib/dump_cleaner/cleanup/cleaning_steps/randomize_formatted_number_spec.rb
@@ -0,0 +1,84 @@
+require "spec_helper"
+
+RSpec.describe DumpCleaner::Cleanup::CleaningSteps::RandomizeFormattedNumber do
+  def step_context(orig_value:, record: { "id_value" => "123" }, type: "some_type", cleanup_data: [])
+    DumpCleaner::Cleanup::StepContext.new(orig_value:, record:, type:, cleanup_data:)
+  end
+
+  def cleaner(step_context)
+    described_class.new(step_context)
+  end
+
+  describe "#run" do
+    it "returns the step_context" do
+      step_context = step_context(orig_value: "1")
+      expect(cleaner(step_context).run(format: '(?<x>\d)')).to eq(step_context)
+    end
+
+    it "raises error if format has no named group 'x'" do
+      step_context = step_context(orig_value: "1")
+      expect { cleaner(step_context).run(format: '(?<some>\d)') }
+        .to raise_error(ArgumentError, /named group starting with "x"/)
+    end
+
+    it "raises error if named groups in the format do not match the whole value" do
+      step_context = step_context(orig_value: "12 34")
+      expect { cleaner(step_context).run(format: '(?<some>\d{2}) (?<x>\d{2})') }
+        .to raise_error(ArgumentError, /whole value/)
+    end
+
+    it "raises error if an 'x*' named group matches a non-number" do
+      step_context = step_context(orig_value: "12a34")
+      expect { cleaner(step_context).run(format: "(?<some>.{2})(?<x>.*)") }
+        .to raise_error(ArgumentError, /must match numbers only/)
+    end
+
+    it "warns and returns nil if the current value does not match the format" do
+      step_context = step_context(orig_value: "123456")
+
+      expect(DumpCleaner::Log).to receive(:warn)
+      expect(cleaner(step_context).run(format: '(?<some>\d{2})(?<x1>\d{2})').current_value).to be_nil
+    end
+
+    it "returns the current_value number as a string" do
+      expect(cleaner(step_context(orig_value: "1")).run(format: '(?<x>\d)').current_value).to be_a(String)
+    end
+
+    it "returns a formatted with the same length" do
+      step_context = step_context(orig_value: "1234567")
+      expect(cleaner(step_context).run(format: '(?<some>\d{1})(?<x>\d{6})').current_value.length).to eq(7)
+    end
+
+    it "randomizes the numbers in the 'x*' named captures but not others" do
+      step_context = step_context(orig_value: "123456")
+      result = cleaner(step_context).run(format: '(?<some>\d{3})(?<x>\d{3})').current_value
+
+      expect(result[0...3]).to eq("123")
+      expect(result).not_to eq("123456")
+      expect(result.to_i).to be_between(123_000, 123_999)
+    end
+
+    it "supports multiple groups named 'x*'" do
+      step_context = step_context(orig_value: "123 456 789")
+      format = '(?<x1>\d{3})(?<space1> )(?<irrelevant>\d{3})(?<space2> )(?<x3>\d{3})'
+      result = cleaner(step_context).run(format:).current_value
+
+      expect(result[0...3]).to match(/\d{3}/)
+      expect(result[0...3]).not_to eq("123")
+      expect(result[4...7]).to eq("456")
+      expect(result[8...11]).to match(/\d{3}/)
+      expect(result[8...11]).not_to eq("789")
+    end
+
+    it "returns a deterministic random number" do
+      result1 = cleaner(step_context(orig_value: "123456")).run(format: '(?<some>\d{3})(?<x1>\d{3})').current_value
+      result2 = cleaner(step_context(orig_value: "123456")).run(format: '(?<some>\d{3})(?<x1>\d{3})').current_value
+      expect(result1).to eq(result2)
+
+      result3 = cleaner(step_context(orig_value: "654321")).run(format: '(?<some>\d{3})(?<x1>\d{3})').current_value
+      result4 = cleaner(step_context(orig_value: "654321")).run(format: '(?<some>\d{3})(?<x1>\d{3})').current_value
+      expect(result3).to eq(result4)
+      expect(result3).not_to eq(result1)
+    end
+  end
+end
